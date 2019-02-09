2013e1a615daf5053b59a1f722535fc61445b33f
Jacky <jackymayonaise@gmail.com>
2019-02-09 18:59:51 -0500

    Add basic frontend structure

---
+++ frontend/FoodForm.js
@@ -0,0 +1,19 @@
+class FoodForm extends React.Components{
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render (){
+    return (
+      <form>
+        <input type="input"> 
+        <input type="submit">
+      </form>
+    );
+  }
+
+
+}
