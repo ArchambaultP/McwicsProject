2076157d6f509bdbed6bc142f03d2accfbc62f55
Jacky <jackymayonaise@gmail.com>
2019-02-10 01:25:18 -0500

    Front end test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
+import FoodInput from './FoodInput';
 import logo from './logo.svg';
 import './App.css';
+import M from 'materialize-css';
 
-class App extends Component {
+const foods =  {'Banana': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34khhDou8YgsAKBHsl5ROXrgc0q2Y4b56IzwAQp05-hjCZAuq', 'Chocolate':null, 'Strawberry':null, 'Kale':null,
+                  'Potatoe':null, 'Carrot':null, 'Onion':null, 'Beet':null, 'Artichoke':null, 
+                    'Garlic':null, 'Chili Peppers':null, 'Tomato':null} 
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {suggestions: foods};
+    this.handleTextInput = this.handleTextInput.bind(this);
+//		this.handleKeyPress = this.handleKeyPress.bind(this);
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.autocomplete');
+      var instances = M.Autocomplete.init(elems, { data: foods, limit: 10, onAutoComplete: () => console.log("AUTOCOMPLETED")});
+    });
+  }
+  
+  handleTextInput(event) {
+    console.log(event.target.value);
+  }
+  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form onSubmit={e => { e.preventDefault(); }}>
+				<h1> Fridge2Meals </h1>
+        <div className='row'>
+          <FoodInput onChange={text => this.handleTextInput(text)}/>
+        </div>
+				<div id='items' className='row'>
+				</div>
+
+      </form>
     );
   }
 }
---
+++ frontend/src/FoodInput.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class FoodInput extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			query: ''
+		}
+	}
+
+	updateText(event){
+		this.setState({query: event.target.value});
+		this.props.onChange(event);
+	}
+
+	render (){
+		return (
+			<div className='input-field col s12'>
+				<input placeholder="Enter ingredients" className='autocomplete' type='text' onChange={
+					event => this.updateText(event) }
+					value={this.state.query} />
+			</div>
+		)
+
+	}
+};
+
+export default FoodInput;
